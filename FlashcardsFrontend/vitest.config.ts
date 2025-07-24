5dbf6643b0dfb139e374e1c61a7dc8d53002d955
Michałowicz <michal.michalowicz@capgemini.com>
2025-07-24 14:15:15 +0200

    Change due to GutHub actions error

--- FlashcardsFrontend/vitest.config.ts
+++ FlashcardsFrontend/vitest.config.ts
@@ -1,11 +1,13 @@
-
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig(
 {
-  test: 
-  {
+  test: {
     environment: 'jsdom',
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
