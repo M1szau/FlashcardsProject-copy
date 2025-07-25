933f54f06b017308b75848e7ecd70dfc6051fb0a
M1szau <hades17777@wp.pl>
2025-07-25 08:47:56 +0200

    Added Header test

---
+++ FlashcardsFrontend/src/__tests__/Header.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import '@testing-library/jest-dom';
+import Header from '../components/Header';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Header component', () =>
+{
+    it('Renders image and children', () =>
+    {
+        render(
+            <MemoryRouter>
+                <Header image = {{src: 'test.png', alt: 'Test image'}}>
+                    <h1>Test Header</h1>
+                </Header>
+            </MemoryRouter>
+        )
+    });
+
+    //Image check
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'test.png');
+    expect(img).toHaveAttribute('alt', 'Test image');
+
+    //Children check
+    expect(screen.getByText(/Test Header/i)).toBeInTheDocument();
+});
